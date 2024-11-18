7b0d1a1a5ecee1fada7fba9ca9978a8cf4595c4e
Ashish <thapa09876543211@gmail.com>
2024-11-18 22:21:23 +0545

    hamburger right pareko

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Navigation   HamBurger 
+// Navigation [HamBurger] for less than 768px 
 
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger");
